9137cb6c8def0c027f53eed1361e85cb5b451824
Kamlesh Patel <kamlesh.patel@icreon.com>
2018-09-26 12:44:51 +0530

    Basic Setup

---
+++ HaptiX.RAMHX.Management/HaptiX.RAMHX.Management/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace HaptiX.RAMHX.Management.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
